2a3e9539e04f376f8fce78320d6bebff4346f0df
mseti tech <msetitech@yahoo.com>
2026-02-04 21:35:56 +0300

    fixed nav

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -41,7 +41,9 @@ export default function Navbar() {
 	return (
 		<nav
 			className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
-				isScrolled ? "bg-white shadow-lg py-2" : "bg-transparent py-4"
+				isScrolled
+					? "bg-white shadow-lg py-2"
+					: "bg-gradient-to-r from-[#EE048B] to-[#68226A] py-4"
 			}`}>
 			<div className="max-w-6xl mx-auto px-4">
 				<div className="flex items-center justify-between h-16">

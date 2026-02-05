54e6235187613552002975c27e08e2aa4af3a954
mseti tech <msetitech@yahoo.com>
2026-02-06 01:13:29 +0300

    fixing images

--- frontend/src/components/Sections/Hero.jsx
+++ frontend/src/components/Sections/Hero.jsx
@@ -2,12 +2,13 @@ import { useTranslation } from "react-i18next";
 import { useState, useEffect } from "react";
 import { ChevronDown, Search } from "lucide-react";
 
+import img00 from "../../assets/images/00.jpg";
 import img01 from "../../assets/images/01.jpg";
 import img02 from "../../assets/images/02.jpg";
 import img03 from "../../assets/images/03.jpg";
 import img04 from "../../assets/images/04.jpg";
 
-const images = [img01, img02, img03, img04];
+const images = [img00, img01, img02, img03, img04];
 
 export default function Hero() {
 	const { t } = useTranslation();
